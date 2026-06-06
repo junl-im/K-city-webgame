d396ca12c3e8d60b9876f0a73adf7e4db1a50cb3
junl-im <mcwoogi@gmail.com>
2026-06-06 23:18:09 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/K-city-webgame/',   // ← 이 부분이 중요합니다!
-  server: {
-    port: 5173
-  }
+  base: '/K-city-webgame/',
 });
